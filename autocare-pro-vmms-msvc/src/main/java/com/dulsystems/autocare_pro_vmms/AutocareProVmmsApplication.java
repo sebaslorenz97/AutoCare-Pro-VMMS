496c58935e5711a3e4ea37b5ca38308eec660952
David Lorenzo <david.lorenzo@accenture.com>
2026-06-11 15:33:52 -0700

    First commit of autocare-pro-vmms-msvc

---
+++ autocare-pro-vmms-msvc/src/main/java/com/dulsystems/autocare_pro_vmms/AutocareProVmmsApplication.java
@@ -0,0 +1,13 @@
+package com.dulsystems.autocare_pro_vmms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutocareProVmmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutocareProVmmsApplication.class, args);
+	}
+
+}
---
+++ autocare-pro-vmms-msvc/src/test/java/com/dulsystems/autocare_pro_vmms/AutocareProVmmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dulsystems.autocare_pro_vmms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutocareProVmmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
